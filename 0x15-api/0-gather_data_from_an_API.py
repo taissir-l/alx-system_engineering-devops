231ba7c621602782ae54a269d0206c087e29220c
“taissir-l” <taissirlassoued1@gmail.com>
2024-05-20 09:15:18 -0700

    tasks

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""returns information about his/her TODO list progress
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    try:
+        id = int(sys.argv[1])
+        url = "https://jsonplaceholder.typicode.com"
+        res = requests.get('{}/users/{}'.format(url, id)).json()
+        res_todo = requests.get("{}/todos".format(url),
+                                params={"userId": id}).json()
+        task_comp = [t.get('title') for t in res_todo
+                     if t.get("completed") is True]
+        b = "Employee {} is done with tasks({}/{}):".format(
+            res.get('name'), len(task_comp), len(res_todo))
+        print(b)
+
+        for t in task_comp:
+            print("\t {}".format(t))
+    except ValueError:
+        pass
